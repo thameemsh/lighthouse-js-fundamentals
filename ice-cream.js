99005f982ad23a0029b3122fe642ef8762116a6f
Mohamed Thameem <thameemsh@gmail.com>
2021-10-10 18:36:04 +0000

    ice-cream array exercise

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// iceCreamNewFlavours = iceCreamFlavours.concat("root beer");
+
+iceCreamNewFlavours = [...iceCreamFlavours, "root beer"];
+
+console.log(iceCreamNewFlavours);
+
+console.log(iceCreamNewFlavours[0]);
+
+console.log(iceCreamNewFlavours[-1]);
+
+console.log(iceCreamNewFlavours.length);
+
